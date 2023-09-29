1c5ccf4120fc9cf88e49bb020581df0d21c88de4
John Mburu <johnnkonge2020@gmail.com>
2023-09-29 08:49:55 +0300

    trial

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const text = document.getElementById('text').textContent
+
+document.addEventListener('DOMContentLoaded',function(){
+    text.textContent = "This is really cool!"
+});
